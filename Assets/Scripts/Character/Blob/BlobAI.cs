a1cda18fb32f8a1af1100fbc98db76bffdb3bb85
Morchul <morchul.dev@gmail.com>
2023-07-18 00:32:15 +0200

    add materials

--- Assets/Scripts/Character/Blob/BlobAI.cs
+++ Assets/Scripts/Character/Blob/BlobAI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BlobAI : CharacterBaseAI
@@ -13,43 +11,47 @@ public class BlobAI : CharacterBaseAI
         base.Start();
 
         BTFindTargetObject findFood = new BTFindTargetObject(this, "Food", agent.transform, 10);
-        BTFindTargetObject findFoodContainer = new BTFindTargetObject(this, "FoodContainer", agent.transform, 10);
 
-        BTSequence pickUpNearbyFoodSequence = new BTSequence("PickUpNearbyFood", findFood, moveToTargetObject, pickUpAction);
-        BTSuccessToRunning continuesPickUpNearbyFoodSequence = new BTSuccessToRunning(pickUpNearbyFoodSequence);
+        BTSequence eatSequence = new BTSequence("Blob eat sequence", findFood, moveToTargetObject, eatAction);
 
-        BTCheckTargetObject ifTargetObjectIsEdible = new BTCheckTargetObject(this, ComponentIDs.EDIBLE);
-
-        BTNode setCarriedObjectAsTarget = new BTNode("Set carried- as targetobject", () =>
-        {
-            if (carry.CarriedItem != null)
-            {
-                TargetObject = carry.CarriedItem;
-                return AbstractBTNode.BTStatus.SUCCESS;
-            }
-            else
-                return AbstractBTNode.BTStatus.FAILURE;
-
-        });
-
-        BTSuccessToFailure dropItemBecauseItIsWrong = new BTSuccessToFailure(dropAction);
-
-        BTSelector checkIfCarriedObjectIsEdible = new BTSelector("checkIfCarriedObjectIsEdible", ifTargetObjectIsEdible, dropItemBecauseItIsWrong);
-        BTSequence setAndCheckCarriedItemIsEdible = new BTSequence("setAndCheckCarriedItem", setCarriedObjectAsTarget, checkIfCarriedObjectIsEdible);
-
-        BTSequence takeFoodFromStorage = new BTSequence("take food from storage", findFoodContainer, moveToTargetObject, takeOutAction);
-        BTSuccessToRunning continuesTakeFoodFromStorage = new BTSuccessToRunning(takeFoodFromStorage);
-
-        BTSequence eatFoodInHandSequence = new BTSequence("eat food in hand", setAndCheckCarriedItemIsEdible, eatAction);
-
-        BTSelector eatSelector = new BTSelector("eat", eatFoodInHandSequence, continuesPickUpNearbyFoodSequence, continuesTakeFoodFromStorage);
-
-        IPlan eatFoodPlan = new BTRoot(eatSelector, this);
+        IPlan eatFoodPlan = new BTRoot(eatSequence, this);
         Decision eatFood = new Decision(eatFoodPlan, (_) => 1f);
 
         State idleState = new State("Blob Idle");
         idleState.AddDecision(eatFood);
 
         stateMachine.SetState(idleState);
+
+
+        //BTFindTargetObject findFoodContainer = new BTFindTargetObject(this, "FoodContainer", agent.transform, 10);
+
+        //BTSequence pickUpNearbyFoodSequence = new BTSequence("PickUpNearbyFood", findFood, moveToTargetObject, pickUpAction);
+        //BTSuccessToRunning continuesPickUpNearbyFoodSequence = new BTSuccessToRunning(pickUpNearbyFoodSequence);
+
+        //BTCheckTargetObject ifTargetObjectIsEdible = new BTCheckTargetObject(this, ComponentIDs.EDIBLE);
+
+        //BTNode setCarriedObjectAsTarget = new BTNode("Set carried- as targetobject", () =>
+        //{
+        //    if (carry.CarriedItem != null)
+        //    {
+        //        TargetObject = carry.CarriedItem;
+        //        return AbstractBTNode.BTStatus.SUCCESS;
+        //    }
+        //    else
+        //        return AbstractBTNode.BTStatus.FAILURE;
+
+        //});
+
+        //BTSuccessToFailure dropItemBecauseItIsWrong = new BTSuccessToFailure(dropAction);
+
+        //BTSelector checkIfCarriedObjectIsEdible = new BTSelector("checkIfCarriedObjectIsEdible", ifTargetObjectIsEdible, dropItemBecauseItIsWrong);
+        //BTSequence setAndCheckCarriedItemIsEdible = new BTSequence("setAndCheckCarriedItem", setCarriedObjectAsTarget, checkIfCarriedObjectIsEdible);
+
+        //BTSequence takeFoodFromStorage = new BTSequence("take food from storage", findFoodContainer, moveToTargetObject, takeOutAction);
+        //BTSuccessToRunning continuesTakeFoodFromStorage = new BTSuccessToRunning(takeFoodFromStorage);
+
+        //BTSequence eatFoodInHandSequence = new BTSequence("eat food in hand", setAndCheckCarriedItemIsEdible, eatAction);
+
+        //BTSelector eatSelector = new BTSelector("eat", eatFoodInHandSequence, continuesPickUpNearbyFoodSequence, continuesTakeFoodFromStorage);
     }
 }
